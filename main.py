73685ee97e8b6cbb4cbf4475c6e70a88e0e45931
Elhai12 <elhais12@gmail.com>
2024-12-28 22:10:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -32,21 +32,29 @@ list_dates = [datetime.strftime(d,'%Y-%m-%d') for d in range_date]
 if tiker !='':
 
     tiker = tiker
-    st.write(f"""
-    The symbol was chosen is : {tiker}
-    \nThe range date for history data is : from {' to '.join(list_dates)}
-    """)
-    last_price, previous_price, chg_day = Function.data_today(tiker,key_code)
-    st.markdown(f"## The last price today: {last_price}")
-    st.markdown(f"## The previous day price: {previous_price}")
-    if chg_day.startswith("-"):
-        formatted_value = f'<span style="color:red;">&#x2193; {chg_day}</span>'
-    else:
-        formatted_value = f'<span style="color:green;">&#x2191; {chg_day}</span>'
-    st.markdown(f"## The change percentage: {formatted_value}", unsafe_allow_html=True)
+    last_price, previous_price, chg_day_org = Function.data_today(tiker, key_code)
+    with st.container():
+        col1, col2, col3,col4 = st.columns(4)
+        with col1:
+            st.metric(label="Symbol", value=tiker)
+        with col2:
+            st.metric(label="last Price today", value=f"{float(last_price):.2f}")
+        with col3:
+            st.metric(label=" The previous day price", value=f"{float(previous_price):.2f}")
+
+        with col4:
+            chg_day = chg_day_org[:5]+"%"
+            if chg_day.startswith("-"):
+                formatted_value = f'<span style="color:red;">&#x2193; {chg_day}</span>'
+            else:
+                formatted_value = f'<span style="color:green;">&#x2191; {chg_day}</span>'
+            st.markdown(f"##### The change percentage \n{formatted_value}", unsafe_allow_html=True)
 
     tab3, tab4 = st.tabs(['Historical_data', 'Graphs'])
     with tab3:
+        st.write(f"""
+        The range date for history data is : from {' to '.join(list_dates)}
+        """)
         df_history =  Function.get_historical_data(tiker,list_dates[0],list_dates[1],key_code)
         st.dataframe(df_history)
 
