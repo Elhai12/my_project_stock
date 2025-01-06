f042223eef8be8a5a9e4d254d6b673677f622226
Elhai12 <elhais12@gmail.com>
2025-01-06 19:11:35 +0200

    Add functins and finish logics

--- Function.py
+++ Function.py
@@ -2,6 +2,9 @@ import plotly as plt
 import pandas as pd
 import requests
 import pytz
+import yfinance as yf
+import streamlit as st
+import plotly.express as px
 
 def from_utc_israel(utc_time):
     utc = pytz.utc
@@ -10,27 +13,129 @@ def from_utc_israel(utc_time):
     local_time = utc_time_local.astimezone(israel)
     return local_time
 
-def get_historical_data(symbol,start_date,end_date,key_code):
-    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&outputsize=full&symbol={symbol}&apikey={key_code}'
-    res = requests.get(url)
-    data = res.json()
-    dict_data = data.get('Time Series (Daily)')
-    df = pd.DataFrame.from_dict(dict_data, orient='index').astype(float)
-    df.columns = ['open', 'high', 'low', 'close', 'volume']
-    df.index = pd.to_datetime(df.index)
-    df = df.sort_index()
-    df['Daily_change_percent'] = round(df['close'].pct_change(),2)
-    result_df = df.loc[start_date:end_date]
+def check_valid_tiker(tiker):
+    info_stock_data = yf.Ticker(tiker)
+    try:
+        sym = info_stock_data.info['symbol']
+        tiker_check_test = 'Valid'
+        return tiker_check_test,info_stock_data.info
+    except Exception as er:
+        if str(er) == "'symbol'":
+            st.warning("The symbol not valid")
+            tiker_check_test = 'Invalid'
+        else:
+            st.warning(f"Error: {er}")
+            tiker_check_test = 'Valid'
+        return tiker_check_test,None
 
-    return result_df
 
-def data_today(symbol,key_code):
-    url = f'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={symbol}&apikey={key_code}'
-    res = requests.get(url)
-    data = res.json()
-    data_symbol = data["Global Quote"]
-    last_price = data_symbol["05. price"]
-    previous_price = data_symbol["08. previous close"]
-    chg_day = data_symbol["10. change percent"]
-    return last_price,previous_price,chg_day
+def meta_fund_data(info_stock):
+    metadata_cols = [
+        "shortName", "longName", "exchange", "currency", "sector",
+        "industry", "country", "website",'longBusinessSummary']
+
+    fundamentals_cols = [
+        "marketCap", "enterpriseValue", "trailingPE", "forwardPE","bookValue", "dividendYield",
+        "dividendRate", "ebitda", "totalRevenue","revenuePerShare", "freeCashflow", "operatingCashflow",
+        "currentRatio", "quickRatio", "debtToEquity"]
+    df_meta_data = pd.DataFrame.from_dict(info_stock,orient='index').T[metadata_cols].reset_index(drop=True).reset_index(drop=True)
+    df_fundamentals = pd.DataFrame.from_dict(info_stock,orient='index').T[fundamentals_cols].reset_index(drop=True).reset_index(drop=True).map(lambda x: round(x, 2))
+
+    return df_meta_data,df_fundamentals
+
+def check_api(url):
+    try:
+        res = requests.get(url)
+        err = None
+        if res.status_code != 200 or 'API rate limit is' in res.text:
+            err = "The credits required to make an additional query are insufficient. Please try again later"
+            res = None
+    except Exception as ex:
+        err = f"Error: {ex}"
+        res = None
+    return  res,err
+
+#I decided to drop this function in favor of using yfinance for historical data
+# def get_historical_data(tiker,start_date,end_date,key_code):
+#     url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&outputsize=full&symbol={tiker}&apikey={key_code}'
+#     res,err = check_api(url)
+#     if res:
+#         data = res.json()
+#         dict_data = data.get('Time Series (Daily)')
+#         df = pd.DataFrame.from_dict(dict_data, orient='index').astype(float)
+#         df.columns = ['open', 'high', 'low', 'close', 'volume']
+#         df.index = pd.to_datetime(df.index)
+#         df = df.sort_index()
+#         df['Daily_change_percent'] = round(df['close'].pct_change(),2)
+#         result_df = df.loc[start_date:end_date]
+#
+#         return result_df
+#     else:
+#         st.warning(err)
+#         return None
+
+
+def data_today(tiker,key_code):
+    url = f'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={tiker}&apikey={key_code}'
+    res,err = check_api(url)
+    if res:
+        data = res.json()
+        data_tiker = data["Global Quote"]
+        last_price = data_tiker["05. price"]
+        previous_price = data_tiker["08. previous close"]
+        chg_day = data_tiker["10. change percent"]
+        list_data = last_price,previous_price,chg_day
+        return list_data
+    else:
+        st.warning(err)
+        return None
+
+def create_history_df_yf(tiker,start,end):
+
+    info_stock = yf.Ticker(tiker)
+    df_history = info_stock.history(start=start, end=end, interval='1d')
+    df_history = df_history.reindex(pd.date_range(start=df_history.index.min(), end=df_history.index.max(), freq='D'))
+    df_history['Date'] = pd.to_datetime(df_history.index)
+
+    df_history['year'] = df_history['Date'].dt.year
+
+    df_history[['Open','High','Low','Close','Volume']] = df_history[['Open','High','Low','Close','Volume']].ffill()
+    df_history['Daily_change_percent'] = df_history['Close'].pct_change()
+    df_history['chg_month'] = df_history['Close'].pct_change(periods=30)
+    df_history['symbol'] = tiker
+    df_history['Cumulative_Return'] = (1 + df_history['Daily_change_percent']).cumprod() - 1
+    df_history.index = df_history.index.normalize()
+
+
+    return df_history
+
+def create_plot(tiker,history_df,type_plot,index_df=None):
+
+    df_fill = history_df.reindex(pd.date_range(start=history_df.index.min(), end=history_df.index.max(), freq='D'))
+    df_fill['Close'] = df_fill['Close'].ffill()
+    df_fill['chg_month'] = round(df_fill['Close'].pct_change(periods=30),2)
+    df_fill['Date'] = pd.to_datetime(df_fill.index)
+    df_fill['year'] = df_fill['Date'].dt.year
+    df_fill.index = df_fill.index.floor('D')
+
+    if type_plot == 'box':
+        fig_day = px.box(df_fill,x='year',y='Daily_change_percent',title=f"Box plot for daily change percent compare years for {tiker}",
+                         labels={'Daily_change_percent':'Daily change percent'})
+        fig_month = px.box(df_fill,x='year',y='chg_month',title=f"Box plot for monthly change percent compare years for {tiker}",
+                           labels={'chg_month':'Monthly change percent'})
+        return fig_day,fig_month
+
+    elif type_plot == 'line':
+        fig_line = px.line(df_fill, x="Date", y="Close", title=f'Price close over time for {tiker}',labels={"Close": "Close Price"})
+        return fig_line
+
+    elif type_plot == 'compare':
+        index_sym = index_df.symbol.max()
+        df_fill['symbol'] = tiker
+        df_concat = pd.concat([df_fill,index_df],axis=0)
+        fig_compare = px.line(df_concat,x='Date',y='Cumulative_Return',color='symbol',title=f'Compare Daily change {tiker} vs {index_sym}'
+                              ,labels={'Cumulative_Return':'Cumulative Return'})
+        return fig_compare
+
+
 
--- main.py
+++ main.py
@@ -5,17 +5,18 @@ import seaborn as sns
 from datetime import datetime,timedelta
 import Function
 
+#Define secret code
 key_code = st.secrets['my_secret_key']
+
 st.markdown("""
 #                *App for data stock*
 
 """)
-st.write("#### Please choose symbol and range of dates")
-tab1,tab2 = st.tabs(['Symbol','Dates'])
-with tab1:
-    tiker = st.text_input("The symbol",placeholder= "Insert here the symbol, for example: AAPL")
 
-with tab2:
+st.write("#### Please choose symbol and range of dates")
+#Define tabs 1 ,2 to insert tiker and range dates
+tab1,tab2 = st.tabs(['Dates','Symbol'])
+with tab1:
     today = datetime.now()
     week_before = today + timedelta(weeks=-1)
     range_date = st.date_input(
@@ -25,40 +26,90 @@ with tab2:
         format="YYYY-MM-DD",
         max_value=today
     )
+with tab2:
+    tiker = st.text_input("The symbol",placeholder= "Insert here the symbol, for example: AAPL")
+
+
+
+
+
 list_dates = [datetime.strftime(d,'%Y-%m-%d') for d in range_date]
 
+#After the user press enter and the tiker variable inserted
+if tiker:
+#Check if tiker valid
+    tiker_check,info_stock = Function.check_valid_tiker(tiker)
+#Define tab 4,5 for meta data and fundamental data from yfinance
+    if tiker_check=='Valid':
+        tab3,tab4 =st.tabs(['Meta Data Symbol','Fundamental Data'])
+        with tab3:
+            df_meta = Function.meta_fund_data(info_stock)[0]
+            st.dataframe(df_meta)
+        with tab4:
+            df_fund = Function.meta_fund_data(info_stock)[1]
+            st.dataframe(df_fund)
+
+#Present the current daily data using API
+        tiker = tiker.upper()
+        st.markdown("---")
+        st.markdown("Current daily data")
+        data_currently_list = Function.data_today(tiker, key_code)
+
+        if data_currently_list:
+            last_price, previous_price, chg_day_org = data_currently_list
+            with st.container():
+                col1, col2, col3,col4 = st.columns(4)
+                with col1:
+                    st.metric(label="Symbol", value=tiker)
+                with col2:
+                    st.metric(label="last Price today", value=f"{float(last_price):.2f}")
+                with col3:
+                    st.metric(label=" The previous day price", value=f"{float(previous_price):.2f}")
+
+                with col4:
+                    chg_day = chg_day_org[:5]+"%"
+                    if chg_day.startswith("-"):
+                        formatted_value = f'<span style="color:red;">&#x2193; {chg_day}</span>'
+                    else:
+                        formatted_value = f'<span style="color:green;">&#x2191; {chg_day}</span>'
+                    st.markdown(f"##### The change percentage \n{formatted_value}", unsafe_allow_html=True)
+#Define tab 5,6 for historical data and graphs using yfinance library and plotly
+        tab5,tab6 = st.tabs(['Historical Data', 'Graphs'])
+
+        with tab5:
+                st.write(f"""
+                The range date for history data is : from {' to '.join(list_dates)}
+                """)
+                df_history =  Function.create_history_df_yf(tiker,list_dates[0],list_dates[1])
+                if df_history is not None:
+                    st.dataframe(df_history[['Open','High','Low','Close','Volume']])
+
+#Using a custom function I created for generating graphs based on the requested type.
+                    with tab6:
+                        tab6_1, tab6_2,tab6_3 = st.tabs(['Line Over Time','Box plot for years','Compare period with index'])
+                        with tab6_1:
+                            fig_line = Function.create_plot(tiker,df_history,'line')
+                            st.plotly_chart(fig_line)
+                        with tab6_2:
+                            fig_box_day,fig_box_month = Function.create_plot(tiker,df_history,'box')
 
 
-if tiker !='':
+                            st.plotly_chart(fig_box_day)
+                            diff_days = (datetime.strptime(list_dates[1], '%Y-%m-%d') - datetime.strptime(list_dates[0],
+                                                                                                          '%Y-%m-%d')).days
+                            if diff_days>=30:
+                                st.markdown("---")
+                                st.plotly_chart(fig_box_month)
+                        with tab6_3:
+                            input_index = st.selectbox(label='Index to compare',options=['SPY - S&P 500','QQQ - NASDAQ'],placeholder="Choose index to compare")
+                            if input_index:
+                                index_sym = input_index.split("-")[0][:-1]
 
-    tiker = tiker
-    last_price, previous_price, chg_day_org = Function.data_today(tiker, key_code)
-    with st.container():
-        col1, col2, col3,col4 = st.columns(4)
-        with col1:
-            st.metric(label="Symbol", value=tiker)
-        with col2:
-            st.metric(label="last Price today", value=f"{float(last_price):.2f}")
-        with col3:
-            st.metric(label=" The previous day price", value=f"{float(previous_price):.2f}")
+                                history_index = Function.create_history_df_yf(tiker=index_sym, start=list_dates[0],end=list_dates[1])
+                                fig_compare = Function.create_plot(tiker, df_history, 'compare',index_df=history_index)
 
-        with col4:
-            chg_day = chg_day_org[:5]+"%"
-            if chg_day.startswith("-"):
-                formatted_value = f'<span style="color:red;">&#x2193; {chg_day}</span>'
-            else:
-                formatted_value = f'<span style="color:green;">&#x2191; {chg_day}</span>'
-            st.markdown(f"##### The change percentage \n{formatted_value}", unsafe_allow_html=True)
-
-    tab3, tab4 = st.tabs(['Historical_data', 'Graphs'])
-    with tab3:
-        st.write(f"""
-        The range date for history data is : from {' to '.join(list_dates)}
-        """)
-        df_history =  Function.get_historical_data(tiker,list_dates[0],list_dates[1],key_code)
-        st.dataframe(df_history)
+                                st.plotly_chart(fig_compare)
 
 
-else:
-    st.write("Not chosen symbol yet")
+
 
